4f475dc3b8e5993a06c266c4d74405bed5dab7e1
lukenorys <lukenorys@ymail.com>
2021-01-26 16:37:04 -0600

    merge to develop branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var buttonEl = document.querySelector("#save-task");
 console.log(buttonEl);
 
 // for this code blockm, 4 steps were done. 1. create a new task item 2. style the new task item 3. add the text 4. append this element to the task list.
-// going forward, these are the 4 steps that will be key to dynamically creating elements with the DOM
+// going forward, these are the 4 steps that will be key to dynamically creating elements with the DOM.
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
